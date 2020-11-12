daf46fae5f96171578ad595fa0e29542bc79391b
Joe Heath <joe.heath@gmail.com>
2020-11-12 16:33:47 +0000

    more experimentation

---
+++ src/cloc.cpp
@@ -0,0 +1,43 @@
+#include "plugin.hpp"
+
+
+struct Cloc : Module {
+	enum ParamIds {
+		NUM_PARAMS
+	};
+	enum InputIds {
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		PATH880_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		NUM_LIGHTS
+	};
+
+	Cloc() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+	}
+
+	void process(const ProcessArgs& args) override {
+	}
+};
+
+
+struct ClocWidget : ModuleWidget {
+	ClocWidget(Cloc* module) {
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Cloc.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(29.582, 103.563)), module, Cloc::PATH880_OUTPUT));
+	}
+};
+
+
+Model* modelCloc = createModel<Cloc, ClocWidget>("Cloc");
